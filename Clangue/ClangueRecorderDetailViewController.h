1b01dbb45e0408b782fd0ae491ec36010868272c
Villena Guillaume <villenagui.free.fr@gmail.com>
2013-10-27 19:23:06 +0100

    Preparation de l'interface pour l'enregistrement

--- Clangue/ClangueRecorderDetailViewController.h
+++ Clangue/ClangueRecorderDetailViewController.h
@@ -13,6 +13,11 @@
 @property (strong, nonatomic) id detailItem;
 @property (strong, nonatomic) id subjectId;
 @property (weak, nonatomic) IBOutlet UIWebView *enonce;
+@property (weak, nonatomic) IBOutlet UIButton *record;
+@property (weak, nonatomic) IBOutlet UIButton *playAudio;
+@property (weak, nonatomic) IBOutlet UIButton *stopAudio;
+@property (weak, nonatomic) IBOutlet UIButton *sendAction;
+@property (weak, nonatomic) IBOutlet UILabel *timeText;
 
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 @end
